c96533a2f2dfbc1a6f2acc639e0f983d5b1ed9bf
Administrator <Administrator@th002.shgw.local>
2018-11-19 14:23:28 +0800

    小bug修改

--- CIWJN/WebRoot/resources/js/td/newCurve.js
+++ CIWJN/WebRoot/resources/js/td/newCurve.js
@@ -332,7 +332,7 @@ function iview(){
 							imgnum = 3;
 						}
 						$("#m3"+machine[f].fid).html("--");
-						$("#m3"+machine[f].fid).html("--");
+						$("#m2"+machine[f].fid).html("--");
 						for(var k=0;k<welderName.length;k++){
 							if(welderName[k].fid==parseInt(redata.substring(0+i, 4+i),10)){
 								$("#m3"+machine[f].fid).html(welderName[k].fwelder_no);
