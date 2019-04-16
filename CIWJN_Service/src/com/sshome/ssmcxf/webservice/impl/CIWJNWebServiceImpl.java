4575618130226c19775233a895c0f09286c5a473
Clown <Clown@Clown-PC>
2019-04-16 09:25:14 +0800

    文件新增替换（海）

--- CIWJN_Service/src/com/sshome/ssmcxf/webservice/impl/CIWJNWebServiceImpl.java
+++ CIWJN_Service/src/com/sshome/ssmcxf/webservice/impl/CIWJNWebServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sshome.ssmcxf.webservice.CIWJNWebService;
+import com.sshome.ssmcxf.webservice.impl.staticAppliceFClass.AppliceIoc;
 
 import net.sf.json.JSONObject;
 
@@ -19,7 +20,7 @@ import net.sf.json.JSONObject;
 public class CIWJNWebServiceImpl implements CIWJNWebService {
 	@Override
 	public Object enterTheWS(String obj1, String obj2) {
-		try{
+		/*try{
 			JSONObject json1 = JSONObject.fromObject(obj1);
 			ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"config/spring-common.xml"});
 			Class<?>  cls = context.getBean(json1.getString("CLASSNAME")).getClass();
@@ -29,12 +30,19 @@ public class CIWJNWebServiceImpl implements CIWJNWebService {
 		} catch (Exception e){
 			e.printStackTrace();
 			return null;
+		}*/
+		try{
+			Object obj=AppliceIoc.enterTheWS(obj1,obj2);
+			return obj;
+		}catch(Exception e){
+			e.getStackTrace();
+			return null;
 		}
 	}
 	
 	@Override
 	public Object enterTheIDU(String obj1, String obj2) {
-		try{
+		/*try{
 			JSONObject json1 = JSONObject.fromObject(obj1);
 			ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"config/spring-common.xml"});
 			Class<?>  cls = context.getBean(json1.getString("CLASSNAME")).getClass();
@@ -44,22 +52,39 @@ public class CIWJNWebServiceImpl implements CIWJNWebService {
 		} catch (Exception e){
 			e.printStackTrace();
 			return null;
+		}*/
+		try{
+			Object obj=AppliceIoc.enterTheIDU(obj1,obj2);
+			return obj;
+		}catch(Exception e){
+			e.getStackTrace();
+			return null;
 		}
 	}
 
 	@Override
 	public Object enterNoParamWs(String obj1) {
-		try{
+		/*try{
 			JSONObject json1 = JSONObject.fromObject(obj1);
-			ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"config/spring-common.xml"});
+			ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"config/spring-common2.xml"});
 			Class<?>  cls = context.getBean(json1.getString("CLASSNAME")).getClass();
 			Method m = cls.getDeclaredMethod(json1.getString("METHOD"));
 			Object obj = m.invoke(context.getBean(json1.getString("CLASSNAME")));
+	        
 			return obj;
+			
 		} catch (Exception e){
 			e.printStackTrace();
 			return null;
+		}*/
+		try{
+			Object obj=AppliceIoc.enterNoParamWs(obj1);
+			return obj;
+		}catch(Exception e){
+			e.getStackTrace();
+			return null;
 		}
+		
 	}
 
 }
